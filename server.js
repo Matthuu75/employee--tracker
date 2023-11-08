1e6183f698fb155c6c378a5a4dd70d3cc3b23e9e
Marcus Matthews <mlmatthews75@gmail.com>
2023-11-07 19:59:25 -0500

    added more code to server js file

--- server.js
+++ server.js
@@ -62,3 +62,116 @@ function menuPrompt() {
         }
     });
 }
+
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.table(results);
+        menuPrompt();
+    });
+}
+function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+        menuPrompt();
+    });
+}
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+        console.table(results);
+        menuPrompt();
+    });
+}
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role ID of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the manager ID of the employee?'
+        }
+    ]).then(answer => {
+        db.query('INSERT INTO employee SET ?', { first_name: answer.first_name, last_name: answer.last_name, role_id: answer.role_id, manager_id: answer.manager_id }, function (err, results) {
+            if (err) throw err;
+            console.log('Employee added.');
+            menuPrompt();
+        });
+    });
+}
+function addDepartment() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'department',
+        message: 'What is the name of the department?'
+    }).then(answer => {
+        db.query('INSERT INTO department SET ?', { name: answer.department }, function (err, results) {
+            if (err) throw err;
+            console.log('Department added.');
+            menuPrompt();
+        });
+    });
+}
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'What is the department ID of the role?'
+        }
+    ]).then(answer => {
+        db.query('INSERT INTO role SET ?', { title: answer.title, salary: answer.salary, department_id: answer.department_id }, function (err, results) {
+            if (err) throw err;
+            console.log('Role added.');
+            menuPrompt();
+        });
+    });
+}
+function updateRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'What is the ID of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role ID of the employee?'
+        }
+    ]).then(answer => {
+        db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answer.role_id, answer.employee_id], function (err, results) {
+            if (err) throw err;
+            console.log('Role updated.');
+            menuPrompt();
+        });
+    });
+}
+
+menuPrompt();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
