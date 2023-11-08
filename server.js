a8e32183944991fe498afe02006a95541bc3a978
Marcus Matthews <mlmatthews75@gmail.com>
2023-11-07 20:49:42 -0500

    testing/debugging serverjs file

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    database: "employee_tracker",
+    database: "employee_db",
     password: "password",
   },
-  console.log(`Connected to the employee_tracker database.`)
+  console.log(`Connected to the employee_db database.`)
 );
 db.connect((err) => {
   if (err) throw err;
